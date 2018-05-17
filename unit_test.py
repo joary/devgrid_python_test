512e0263f282cbe3c4252ab40ca42fd483ffb3c1
Joary Paulo <joarypl@gmail.com>
2018-05-17 11:39:01 -0300

    Add requirements and remove print from unit_test

--- unit_test.py
+++ unit_test.py
@@ -18,24 +18,24 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_insert_sensor_record(self):
         rv = self.app.post('/api/add_sensor_record/',data=events[0])
-        print(rv.data)
+        #print(rv.data)
         assert rv.status_code == 200
 
     def test_insert_nojson_sensor_record(self):
         rv = self.app.post('/api/add_sensor_record/',data='{}')
-        print(rv.data)
+        #print(rv.data)
         assert rv.status_code == 400
 
     def test_insert_wrong_json_sensor_record(self):
         rv = self.app.post('/api/add_sensor_record/',data='{"record": "nothing"}')
-        print(rv.data)
+        #print(rv.data)
         assert rv.status_code == 400
 
     def test_insert_1k_sensors(self):
         for e in events:
             self.app.post('/api/add_sensor_record/',data=e)
         rv = self.app.get('/api/statistics/')
-        print(rv.data)
+        #print(rv.data)
         assert rv.status_code == 200
 
 if __name__ == '__main__':
